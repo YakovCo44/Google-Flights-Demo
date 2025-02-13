55720f3c60d139842ee713de10c19aa32c11ac0e
Yakov Cohen <yakovco44@gmail.com>
2025-02-13 13:00:42 +0200

    final edit

--- src/services/apiservice.js
+++ src/services/apiservice.js
@@ -89,13 +89,109 @@ export async function searchFlights(departure, arrival, departureDate, returnDat
                     arrival_airport_lat: 32.0, arrival_airport_lng: 34.8  // Tel Aviv (TLV)
                 }
             ]
+        },
+        {
+            price: 350,
+            airline: "British Airways",
+            airline_logo: "https://upload.wikimedia.org/wikipedia/en/thumb/1/1e/British_Airways_Logo.svg/1024px-British_Airways_Logo.svg.png",
+            flights: [
+                {
+                    departure_date: departureDate,
+                    airline: "British Airways",
+                    departure_time: "09:00 AM",
+                    arrival_time: "03:15 PM",
+                    duration: { text: "5h 15m" },
+                    departure_airport_lat: 51.5, departure_airport_lng: -0.1, // London (LHR)
+                    arrival_airport_lat: 32.0, arrival_airport_lng: 34.8  // Tel Aviv (TLV)
+                }
+            ]
+        },
+        {
+            price: 400,
+            airline: "Lufthansa",
+            airline_logo: "https://upload.wikimedia.org/wikipedia/commons/3/30/Lufthansa_Logo_2018.svg",
+            flights: [
+                {
+                    departure_date: departureDate,
+                    airline: "Lufthansa",
+                    departure_time: "12:00 PM",
+                    arrival_time: "05:30 PM",
+                    duration: { text: "4h 30m" },
+                    departure_airport_lat: 50.0, departure_airport_lng: 8.5, // Frankfurt (FRA)
+                    arrival_airport_lat: 32.0, arrival_airport_lng: 34.8  // Tel Aviv (TLV)
+                }
+            ]
+        },
+        {
+            price: 500,
+            airline: "Turkish Airlines",
+            airline_logo: "https://upload.wikimedia.org/wikipedia/commons/a/a6/Turkish_Airlines_logo_2019.svg",
+            flights: [
+                {
+                    departure_date: departureDate,
+                    airline: "Turkish Airlines",
+                    departure_time: "02:30 PM",
+                    arrival_time: "05:45 PM",
+                    duration: { text: "2h 15m" },
+                    departure_airport_lat: 41.0, departure_airport_lng: 28.9, // Istanbul (IST)
+                    arrival_airport_lat: 32.0, arrival_airport_lng: 34.8  // Tel Aviv (TLV)
+                }
+            ]
+        },
+        {
+            price: 600,
+            airline: "Air France",
+            airline_logo: "https://upload.wikimedia.org/wikipedia/commons/4/46/Air_France_Logo.svg",
+            flights: [
+                {
+                    departure_date: departureDate,
+                    airline: "Air France",
+                    departure_time: "07:00 AM",
+                    arrival_time: "01:10 PM",
+                    duration: { text: "6h 10m" },
+                    departure_airport_lat: 48.8, departure_airport_lng: 2.3, // Paris (CDG)
+                    arrival_airport_lat: 32.0, arrival_airport_lng: 34.8  // Tel Aviv (TLV)
+                }
+            ]
+        },
+        {
+            price: 700,
+            airline: "Delta Airlines",
+            airline_logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Delta_logo.svg/1024px-Delta_logo.svg.png",
+            flights: [
+                {
+                    departure_date: departureDate,
+                    airline: "Delta Airlines",
+                    departure_time: "08:00 PM",
+                    arrival_time: "02:45 PM (next day)",
+                    duration: { text: "11h 45m" },
+                    departure_airport_lat: 40.7, departure_airport_lng: -74.0, // New York (JFK)
+                    arrival_airport_lat: 32.0, arrival_airport_lng: 34.8  // Tel Aviv (TLV)
+                }
+            ]
+        },
+        {
+            price: 800,
+            airline: "Emirates",
+            airline_logo: "https://upload.wikimedia.org/wikipedia/commons/b/bd/Emirates_logo.svg",
+            flights: [
+                {
+                    departure_date: departureDate,
+                    airline: "Emirates",
+                    departure_time: "06:45 AM",
+                    arrival_time: "10:15 AM",
+                    duration: { text: "3h 30m" },
+                    departure_airport_lat: 25.2, departure_airport_lng: 55.3, // Dubai (DXB)
+                    arrival_airport_lat: 32.0, arrival_airport_lng: 34.8  // Tel Aviv (TLV)
+                }
+            ]
         }
-    ]    
+    ]        
 
     // ✅ API request options
     const options = {
         method: "GET",
-        url: `https://${process.env.REACT_APP_RAPIDAPI_HOST}/searchFlights`,
+        url: 'https://sky-scrapper.p.rapidapi.com/api/v2/flights/searchFlights',
         params: {
             departure_id: departure.iata,
             arrival_id: arrival.iata,
@@ -138,7 +234,7 @@ export async function searchAirports(query) {
 
     const options = {
         method: "GET",
-        url: 'https://sky-scrapper.p.rapidapi.com/api/v2/flights/searchFlights',
+        url: 'https://sky-scrapper.p.rapidapi.com/api/v1/flights/searchAirport',
         params: { query, locale: "en-US" },
         headers: {
             "X-RapidAPI-Key": API_KEY,
